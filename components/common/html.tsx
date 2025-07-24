4ec7dd7b0b8d8ab9222b0b355a7bc23c7f889bf3
Amin Babu <amin.babu.bd@gmail.com>
2025-07-25 00:21:47 +0600

    chore: add html rendering component

--- components/common/html.tsx
+++ components/common/html.tsx
@@ -1,10 +1,21 @@
+import { cn } from "@/lib/utils";
 import DOMPurify from "isomorphic-dompurify";
 import { ComponentProps } from "react";
 
-const HTML = ({ data, ...props }: { data: string } & ComponentProps<"div">) => {
+const HTML = ({
+  data,
+  className,
+  ...props
+}: { data: string; className?: string } & ComponentProps<"div">) => {
   const sanitizedHtml = DOMPurify.sanitize(data);
 
-  return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} {...props} />;
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+      className={cn("prose max-w-none", className)}
+      {...props}
+    />
+  );
 };
 
 export default HTML;
