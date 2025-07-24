f1b6bb92b2da8ede0b7518711b2ba477068f7a3e
Amin Babu <amin.babu.bd@gmail.com>
2025-07-25 00:21:28 +0600

    chore: update root to redirect to the product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-import Container from "@/components/common/container";
-import Main from "@/components/common/main";
-import Section from "@/components/common/section";
-import Benefits from "@/components/pages/products/benefits";
-import Details from "@/components/pages/products/details";
-import Features from "@/components/pages/products/featrues";
-import Instructors from "@/components/pages/products/instructors";
-import Intro from "@/components/pages/products/intro";
-import Sidebar from "@/components/pages/products/sidebar";
-import Summary from "@/components/pages/products/summary";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <Main>
-      <Section>
-        <Container className="grid grid-cols-12 items-start gap-y-8 md:gap-x-8 lg:gap-x-12">
-          <div className="col-span-8">
-            <Intro />
-            <Instructors />
-            <Summary />
-            <Benefits />
-            <Features />
-            <Details />
-          </div>
-          <div className="col-span-4 lg:sticky lg:top-6">
-            <Sidebar />
-          </div>
-        </Container>
-      </Section>
-    </Main>
-  );
+  return redirect("/products/ielts-course");
 }
