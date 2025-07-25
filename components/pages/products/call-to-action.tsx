500c2a9a7ad244263d16b38a8f4f3435a721c67f
Amin Babu <amin.babu.bd@gmail.com>
2025-07-25 22:10:42 +0600

    chore: finish call to action component

--- components/pages/products/call-to-action.tsx
+++ components/pages/products/call-to-action.tsx
@@ -1,5 +1,24 @@
-const CallToAction = () => {
-  return <div>CallToAction</div>;
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ComponentProps, ReactNode } from "react";
+
+interface IProps extends ComponentProps<"button"> {
+  children: ReactNode;
+  className?: string;
+}
+
+const CTAButton = ({ children, className, ...props }: IProps) => {
+  return (
+    <Button
+      className={cn(
+        "cursor-pointer bg-green-600 text-base font-medium shadow-[0_4px_0_0] shadow-green-800 hover:bg-green-700 hover:shadow-green-700",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
 };
 
-export default CallToAction;
+export default CTAButton;
