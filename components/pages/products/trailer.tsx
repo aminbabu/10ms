60ec74c2f2ae1dcf4d549141f2cdb8f0bf4b96d5
Amin Babu <amin.babu.bd@gmail.com>
2025-07-25 13:20:23 +0600

    chore: finish trailers component

--- components/pages/products/trailer.tsx
+++ components/pages/products/trailer.tsx
@@ -1,5 +1,68 @@
-const Trailer = () => {
-  return <div>Trailer</div>;
+"use client";
+
+import PlayButton from "@/components/common/play-button";
+import { IMedia } from "@/interfaces/product";
+import Image from "next/image";
+import { memo, useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
+interface IProps {
+  data: IMedia;
+  isActive: boolean;
+}
+
+const Trailer = ({ data, isActive }: IProps) => {
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    if (!isActive && playing) {
+      setPlaying(false);
+    }
+  }, [isActive, playing]);
+
+  const handlePlay = () => {
+    setPlaying(true);
+  };
+
+  const videoUrl = `https://www.youtube.com/watch?v=${data.resource_value}`;
+
+  const playIcon = (
+    <div className="bg-foreground/40 absolute inset-0 flex items-center justify-center">
+      <PlayButton onPlay={handlePlay} />
+    </div>
+  );
+
+  return (
+    <div className="bg-foreground relative aspect-video">
+      {data?.resource_type === "video" ? (
+        <>
+          <ReactPlayer
+            src={videoUrl}
+            playing={playing}
+            controls
+            light={data.thumbnail_url ?? true}
+            width="100%"
+            height="100%"
+            playIcon={playIcon}
+            config={{
+              youtube: {
+                autoplay: playing ? 1 : 0,
+                start: 0,
+              },
+            }}
+          />
+        </>
+      ) : (
+        <Image
+          src={data.resource_value}
+          alt={data.name}
+          width={480}
+          height={270}
+          className="h-full w-full object-cover"
+        />
+      )}
+    </div>
+  );
 };
 
-export default Trailer;
+export default memo(Trailer);
--- components/pages/products/trailers.tsx
+++ components/pages/products/trailers.tsx
@@ -1,5 +1,106 @@
-const Trailers = () => {
-  return <div>Trailers</div>;
+"use client";
+
+import Trailer from "@/components/pages/products/trailer";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { IMedia } from "@/interfaces/product";
+import { cn } from "@/lib/utils";
+import { UseEmblaCarouselType } from "embla-carousel-react";
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+
+const Trailers = ({
+  data,
+  className,
+}: {
+  data: Array<IMedia>;
+  className?: string;
+}) => {
+  const [api, setApi] = useState<UseEmblaCarouselType[1] | null>(null);
+  const [thumbApi, setThumbApi] = useState<UseEmblaCarouselType[1] | null>(
+    null,
+  );
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!api || !thumbApi) return;
+      api.scrollTo(index);
+    },
+    [api, thumbApi],
+  );
+
+  const onSelect = useCallback(() => {
+    if (!api || !thumbApi) return;
+
+    const currentSlide = api.selectedScrollSnap();
+
+    setSelectedIndex(currentSlide);
+    thumbApi.scrollTo(currentSlide);
+  }, [api, thumbApi, setSelectedIndex]);
+
+  // Watch for active slide changes
+  useEffect(() => {
+    if (!api) return;
+
+    onSelect();
+
+    api.on("select", onSelect).on("reInit", onSelect);
+
+    return () => {
+      api.off("select", onSelect);
+    };
+  }, [api, onSelect]);
+
+  return (
+    <div className={cn("space-y-2.5", className)}>
+      <Carousel className="w-full" setApi={setApi}>
+        <CarouselContent>
+          {data.map((item, idx) => (
+            <CarouselItem key={item.resource_value}>
+              <Trailer data={item} isActive={selectedIndex === idx} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-2" />
+        <CarouselNext className="right-2" />
+      </Carousel>
+      <Carousel setApi={setThumbApi} className="px-2">
+        <CarouselContent className="-ml-2">
+          {data.map((item, idx) => (
+            <CarouselItem
+              key={item.resource_value}
+              className="aspect-video basis-1/6 cursor-pointer pl-2"
+              onClick={() => onThumbClick(idx)}
+            >
+              <Image
+                src={
+                  item.resource_type === "video"
+                    ? item.thumbnail_url
+                    : item.resource_value
+                }
+                alt={item.name}
+                width={160}
+                height={90}
+                className={cn(
+                  "hover:border-border h-full w-full border-2 border-transparent object-cover transition-all duration-300",
+                  {
+                    "rounded border-green-500 hover:border-green-500":
+                      idx === selectedIndex,
+                  },
+                )}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Trailers;
