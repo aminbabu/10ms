42969b214d548b608db7be41f0cf53f97324cd0c
Amin Babu <amin.babu.bd@gmail.com>
2025-07-26 23:53:56 +0600

    chore: remove unused components and pages

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { routing } from "@/i18n/routing";
-import { permanentRedirect } from "next/navigation";
-
-export default function RootPage() {
-  const { defaultLocale } = routing;
-
-  permanentRedirect(`/${defaultLocale}`);
-}
